d7b11306c6f00bdf8bad3692fccd5923f023fc36
danm <dan@livekit.io>
2025-06-13 10:16:18 -0600

    initial commit

---
+++ agent.py
@@ -0,0 +1,7 @@
+from src.core.greeting.say_hello import say_hello
+
+def main():
+    say_hello()
+
+if __name__ == "__main__":
+    main()
---
+++ src/core/greeting/say_hello.py
@@ -0,0 +1,8 @@
+def say_hello():
+    """
+    A simple function that prints a greeting message.
+    """
+    print("Hello from the greeting module!")
+
+if __name__ == "__main__":
+    say_hello() 
